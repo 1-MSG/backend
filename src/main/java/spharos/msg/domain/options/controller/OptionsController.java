9cb9556057bd92ee8dbc854a2b06b3f669ddae8d
chojunho <chojunho0310@naver.com>
2024-04-01 17:00:57 +0900

    feat : 옵션 스웨거 설명 추가 #83

--- src/main/java/spharos/msg/domain/options/controller/OptionsController.java
+++ src/main/java/spharos/msg/domain/options/controller/OptionsController.java
@@ -1,5 +1,7 @@
 package spharos.msg.domain.options.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import spharos.msg.domain.options.service.OptionsService;
@@ -7,23 +9,28 @@ import spharos.msg.global.api.ApiResponse;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Options", description = "옵션 관련 API")
 @RequestMapping("/api/v1/option")
 public class OptionsController {
     private final OptionsService optionsService;
-
+    @Operation(summary = "옵션 종류 조회",
+            description = "상품의 옵션 종류와 레벨을 조회 합니다")
     @GetMapping
     public ApiResponse<?> getOptionType(
             @RequestParam(value = "optionIds") Long[] optionIds
     ){
         return optionsService.getOptionType(optionIds);
     }
-
+    @Operation(summary = "옵션 첫번째 항목 조회",
+            description = "상품이 가지는 옵션의 첫번째 항목을 조회합니다.")
     @GetMapping("/first-option")
     public ApiResponse<?> getFirstOption(
             @RequestParam(value = "optionIds") Long[] optionIds
     ){
         return optionsService.getFirstOption(optionIds);
     }
+    @Operation(summary = "자식 옵션 조회",
+            description = "옵션을 선택했을 때 해당 옵션의 자식 옵션을 조회 합니다.")
     @GetMapping("/optionName/{optionId}")
     public ApiResponse<?> getOptionChild(
             @PathVariable Long optionId)
