62554d1353f9a4bc343f0f52f1374edd9b9dea1c
chojunho <chojunho0310@naver.com>
2024-04-01 16:53:01 +0900

    feat : 브랜드 스웨거 설명 추가 #106

--- src/main/java/spharos/msg/domain/brand/controller/BrandController.java
+++ src/main/java/spharos/msg/domain/brand/controller/BrandController.java
@@ -1,5 +1,7 @@
 package spharos.msg.domain.brand.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,10 +11,13 @@ import spharos.msg.global.api.ApiResponse;
 
 @RestController
 @RequestMapping("/api/v1/brand")
+@Tag(name = "Brand", description = "브랜드 API")
 @RequiredArgsConstructor
 public class BrandController {
     private final BrandService brandService;
 
+    @Operation(summary = "전체 브랜드 조회",
+            description = "모든 브랜드 이름을 조회합니다")
     @GetMapping
     public ApiResponse<?> getBrands(){
         return brandService.getBrands();
