97014de832806cc63f6bc4e3e90e3aa2b198516a
chojunho <chojunho0310@naver.com>
2024-04-01 14:26:24 +0900

    feat : 중복값 처리

--- src/main/java/spharos/msg/domain/brand/service/BrandService.java
+++ src/main/java/spharos/msg/domain/brand/service/BrandService.java
@@ -7,6 +7,9 @@ import spharos.msg.domain.brand.repository.BrandRepository;
 import spharos.msg.global.api.ApiResponse;
 import spharos.msg.global.api.code.status.SuccessStatus;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -15,10 +18,18 @@ import java.util.stream.Collectors;
 public class BrandService {
     private final BrandRepository brandRepository;
     public ApiResponse<?> getBrands() {
-        return ApiResponse.of(SuccessStatus.BRAND_GET_SUCCESS,
-                 brandRepository.findAll()
-                        .stream()
-                        .map(BrandResponseDto::new)
-                        .collect(Collectors.toSet()));
+        List<BrandResponseDto> brandResponseDtos = brandRepository.findAll()
+                .stream()
+                .map(BrandResponseDto::new)
+                .toList();
+
+        // 중복 체크를 위한 Set 생성
+        Set<String> distinctBrandResponseDtos = new HashSet<>();
+
+        for(BrandResponseDto brandResponseDto:brandResponseDtos){
+            distinctBrandResponseDtos.add(brandResponseDto.getBrandName());
+        }
+
+        return ApiResponse.of(SuccessStatus.BRAND_GET_SUCCESS, distinctBrandResponseDtos);
     }
 }
