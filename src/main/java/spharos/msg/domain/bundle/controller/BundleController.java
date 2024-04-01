ed2259c5f4ca703ee5953ac53219c486cccfde1d
SEOIHYEON <tjwn1408@naver.com>
2024-04-01 14:59:19 +0900

    chore: 특가상품 상품들 조회 api path 변경#97

--- src/main/java/spharos/msg/domain/bundle/controller/BundleController.java
+++ src/main/java/spharos/msg/domain/bundle/controller/BundleController.java
@@ -35,7 +35,7 @@ public class BundleController {
 
     @Operation(summary = "특정 특가상품에 속하는 상품들 조회",
         description = "하나의 특가 상품에 속한 상품들을 배열로 반환합니다")
-    @GetMapping("/{bundleId}/bundleproducts")
+    @GetMapping("/bundles/{bundleId}")
     public ApiResponse<List<Long>> getBundleProducts(
         @PathVariable("bundleId") Long bundleId
     ) {
