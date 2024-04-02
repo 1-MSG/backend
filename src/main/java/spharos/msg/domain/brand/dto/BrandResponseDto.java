5556ec1b920bde1cf1c1a1afe316a15705d1bdca
chojunho <chojunho0310@naver.com>
2024-04-02 09:25:49 +0900

    fix : dto에 EqualsAndHashCode 어노테이션 추가

--- src/main/java/spharos/msg/domain/brand/dto/BrandResponseDto.java
+++ src/main/java/spharos/msg/domain/brand/dto/BrandResponseDto.java
@@ -1,11 +1,13 @@
 package spharos.msg.domain.brand.dto;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import spharos.msg.domain.brand.entity.Brand;
 
 @Getter
 @NoArgsConstructor
+@EqualsAndHashCode
 public class BrandResponseDto {
     private String brandName;
     public BrandResponseDto(Brand brand){
--- src/main/java/spharos/msg/domain/brand/service/BrandService.java
+++ src/main/java/spharos/msg/domain/brand/service/BrandService.java
@@ -2,34 +2,25 @@ package spharos.msg.domain.brand.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import spharos.msg.domain.brand.dto.BrandResponseDto;
 import spharos.msg.domain.brand.repository.BrandRepository;
 import spharos.msg.global.api.ApiResponse;
 import spharos.msg.global.api.code.status.SuccessStatus;
 
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class BrandService {
     private final BrandRepository brandRepository;
+    @Transactional(readOnly = true)
     public ApiResponse<?> getBrands() {
         List<BrandResponseDto> brandResponseDtos = brandRepository.findAll()
                 .stream()
                 .map(BrandResponseDto::new)
+                .distinct()
                 .toList();
-
-        // 중복 체크를 위한 Set 생성
-        Set<String> distinctBrandResponseDtos = new HashSet<>();
-
-        for(BrandResponseDto brandResponseDto:brandResponseDtos){
-            distinctBrandResponseDtos.add(brandResponseDto.getBrandName());
-        }
-
-        return ApiResponse.of(SuccessStatus.BRAND_GET_SUCCESS, distinctBrandResponseDtos);
+        return ApiResponse.of(SuccessStatus.BRAND_GET_SUCCESS, brandResponseDtos);
     }
 }
