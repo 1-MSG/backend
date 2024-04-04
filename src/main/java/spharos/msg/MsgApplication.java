be459d9e738f2fea070bc9d18f05da75be1da810
howudong <tjdvy963@naver.com>
2024-04-04 12:00:12 +0900

    hotfix : CORS 문제 해결

--- src/main/java/spharos/msg/MsgApplication.java
+++ src/main/java/spharos/msg/MsgApplication.java
@@ -4,14 +4,13 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
 @OpenAPIDefinition(
     servers = {
-//        @Server(url = "https://sssg.shop", description = "Default Server URL")
+        @Server(url = "https://sssg.shop", description = "Default Server URL")
     }
 )
 public class MsgApplication {
