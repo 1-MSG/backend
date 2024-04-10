24ea51b2faf530172e28b22d1021cbb6bd4d7989
KangBaekGwa <ksu9801@gmail.com>
2024-04-10 20:11:44 +0900

    perf : 회원가입 시, 필요한 정보만 가져오도록 수정.

--- src/main/java/spharos/msg/domain/users/repository/UsersRepository.java
+++ src/main/java/spharos/msg/domain/users/repository/UsersRepository.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import spharos.msg.domain.users.entity.UserStatus;
 import spharos.msg.domain.users.entity.Users;
@@ -14,6 +16,9 @@ public interface UsersRepository extends JpaRepository<Users, Long> {
     Optional<Users> findByUuid(String uuid);
 
     Optional<Users> findByLoginId(String loginId);
+
+    @Query("SELECT u.status FROM Users u WHERE u.loginId = :loginId")
+    Optional<UserStatus> findStatusByLoginId(@Param("loginId") String loginId);
 
     Optional<Users> findByEmail(String email);
 
