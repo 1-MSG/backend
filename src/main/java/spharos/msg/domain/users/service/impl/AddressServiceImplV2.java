a994fa6025213b9ee41da24d0eb4005c9e72f0b4
KangBaekGwa <ksu9801@gmail.com>
2024-04-10 19:42:56 +0900

    perf : address 추가 시, user 연관관계 설정 N+1 문제 개선

--- src/main/java/spharos/msg/domain/users/service/impl/AddressServiceImplV2.java
+++ src/main/java/spharos/msg/domain/users/service/impl/AddressServiceImplV2.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import spharos.msg.domain.users.converter.AddressConverter;
 import spharos.msg.domain.users.dto.request.AddressRequest;
 import spharos.msg.domain.users.dto.response.AddressResponse;
@@ -13,32 +12,25 @@ import spharos.msg.domain.users.entity.Users;
 import spharos.msg.domain.users.repository.AddressRepository;
 import spharos.msg.domain.users.repository.UsersRepository;
 import spharos.msg.domain.users.service.AddressService;
-import spharos.msg.global.api.code.status.ErrorStatus;
-import spharos.msg.global.api.exception.UsersException;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
-//@Primary
-public class AddressServiceImpl implements AddressService {
+@Primary
+public class AddressServiceImplV2 implements AddressService {
 
     private final AddressRepository addressRepository;
     private final UsersRepository usersRepository;
 
-    @Transactional
     @Override
     public void createAddress(AddressRequest.AddAddressDto dto, Long userId) {
-        Users findUser = usersRepository.findById(userId).orElseThrow(
-                () -> new UsersException(ErrorStatus.DELIVERY_ADDRESS_ADD_FAIL)
-        );
+        Users findUser = usersRepository.getReferenceById(userId);
 
         addressRepository.save(AddressConverter.toEntity(dto, findUser));
     }
 
-    @Transactional(readOnly = true)
     @Override
     public List<AddressResponse.SearchAddressDto> searchAllAddress(Long userId) {
         List<Address> findAddress = addressRepository.findByUsersId(userId);
@@ -46,7 +38,6 @@ public class AddressServiceImpl implements AddressService {
         return AddressConverter.toDto(findAddress);
     }
 
-    @Transactional
     @Override
     public void deleteAddress(Long userId, Long addressId) {
         Address findAddress = addressRepository.findByUsersIdAndId(userId, addressId).orElseThrow();
