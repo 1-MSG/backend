919daf9533b4953b0e3489c7e2de463310a7507b
chojunho <chojunho0310@naver.com>
2024-04-12 14:10:16 +0900

    fix : 브랜드V1,V2 분리 #226

---
+++ src/main/java/spharos/msg/domain/brand/service/BrandServiceV2.java
@@ -0,0 +1,32 @@
+package spharos.msg.domain.brand.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import spharos.msg.domain.brand.dto.BrandDetailDto;
+import spharos.msg.domain.brand.dto.BrandResponseDto;
+import spharos.msg.domain.brand.entity.Brand;
+import spharos.msg.domain.brand.repository.BrandRepository;
+import spharos.msg.global.api.ApiResponse;
+import spharos.msg.global.api.code.status.SuccessStatus;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class BrandServiceV2 {
+    private final BrandRepository brandRepository;
+    public ApiResponse<List<BrandResponseDto>> getBrands() {
+        List<BrandResponseDto> brandResponseDtos = brandRepository.findAll()
+                .stream()
+                .map(BrandResponseDto::new)
+                .distinct()
+                .toList();
+        return ApiResponse.of(SuccessStatus.BRAND_GET_SUCCESS, brandResponseDtos);
+    }
+    public ApiResponse<BrandDetailDto> getBrandDetail(Long brandId) {
+        Brand brand = brandRepository.findById(brandId).orElseThrow();
+        return ApiResponse.of(SuccessStatus.BRAND_DETAIL_GET_SUCCESS, new BrandDetailDto(brand));
+    }
+}
