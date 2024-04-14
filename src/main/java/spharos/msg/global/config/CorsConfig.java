fcc26f79c1ac9fdfa47ab2d8538e63cab890a376
KangBaekGwa <ksu9801@gmail.com>
2024-04-14 22:37:04 +0900

    fix : cors 수정

--- src/main/java/spharos/msg/global/config/CorsConfig.java
+++ src/main/java/spharos/msg/global/config/CorsConfig.java
@@ -8,15 +8,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    /*/
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedMethods(CorsConfiguration.ALL)
+//                .allowedMethods(CorsConfiguration.ALL)
                 .allowedHeaders(CorsConfiguration.ALL)
-                .allowedOriginPatterns(CorsConfiguration.ALL)
+//                .allowedOriginPatterns(CorsConfiguration.ALL)
+
+                //add
+                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://ssgcom-app.vercel.app")
+                .allowedOrigins("https://sssg.shop")
+                .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
+                .maxAge(3600L)
+                //__
+
                 .exposedHeaders(CorsConfiguration.ALL)
                 .allowCredentials(true);
     }
-     */
 }
