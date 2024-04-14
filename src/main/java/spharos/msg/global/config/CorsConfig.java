f07deec2ed47d8706ee128f44f9614795ff5820d
KangBaekGwa <ksu9801@gmail.com>
2024-04-14 18:23:10 +0900

    fix : cors 설정 변경.

--- src/main/java/spharos/msg/global/config/CorsConfig.java
+++ src/main/java/spharos/msg/global/config/CorsConfig.java
@@ -5,11 +5,10 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-/**
- * 이거 설정 다시 한번 보자
- */
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+
+    /*/
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
@@ -19,4 +18,5 @@ public class CorsConfig implements WebMvcConfigurer {
                 .exposedHeaders(CorsConfiguration.ALL)
                 .allowCredentials(true);
     }
+     */
 }
--- src/main/java/spharos/msg/global/config/SecurityConfig.java
+++ src/main/java/spharos/msg/global/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package spharos.msg.global.config;
 
+import java.util.Arrays;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -12,7 +13,9 @@ import org.springframework.security.config.annotation.web.configurers.CsrfConfig
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import spharos.msg.global.security.JwtAuthenticationFilter;
 
 @Configuration
@@ -24,6 +27,7 @@ public class SecurityConfig {
     private final AuthenticationProvider authenticationProvider;
 
     //cors
+    /*/
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         return request -> {
@@ -37,6 +41,20 @@ public class SecurityConfig {
             cors.setAllowCredentials(true);
             return cors;
         };
+    }
+    /*/
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOriginPatterns(List.of("*"));
+        configuration.setAllowedMethods(Arrays.asList("HEAD","POST","GET","DELETE","PUT"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 
