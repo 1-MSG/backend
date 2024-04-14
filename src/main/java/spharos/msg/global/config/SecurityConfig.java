752c9c3b976761d65c75220f516c9eb20412f886
KangBaekGwa <ksu9801@gmail.com>
2024-04-14 22:11:26 +0900

    fix : cors 설정 변경.

--- src/main/java/spharos/msg/global/config/SecurityConfig.java
+++ src/main/java/spharos/msg/global/config/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(
                         authorizeHttpRequests -> authorizeHttpRequests
                                 //Preflight 요청 처리
-                                .requestMatchers(HttpMethod.OPTIONS,"/**").permitAll()
+                                //.requestMatchers(HttpMethod.OPTIONS,"/**").permitAll()
                                 // swagger
                                 .requestMatchers(
                                         "/swagger-ui/**",
@@ -63,147 +63,150 @@ public class SecurityConfig {
                                         "/api-docs/**").permitAll()
                                 .requestMatchers("/error").permitAll()
 
-                                //Auth
-                                .requestMatchers("/api/v1/auth/check-duplicate-id").permitAll()
-                                .requestMatchers("/api/v1/auth/find-id/**").permitAll()
-                                .requestMatchers("/api/v1/auth/login").permitAll()
-                                .requestMatchers("/api/v1/auth/signup").permitAll()
+                                .requestMatchers("/api/v1/**").permitAll()
+                                .requestMatchers("/api/v2/**").permitAll()
 
-                                //Admin에서 회원 탈퇴 기능 사용하기 위해 일시적 허용
-                                //todo : Admin 회원 인증 가능 하면 삭제 필요.
-                                .requestMatchers("/api/v1/auth/secession/**").permitAll()
-
-                                //OAuth (permitAll)
-                                .requestMatchers("/api/v1/oauth/**").permitAll()
-
-                                //User (permitALl)
-                                .requestMatchers("/api/v1/users/**").permitAll()
-
-                                //Address N/A
-                                //비회원 주소 없음
-
-                                //Product
-                                .requestMatchers("/api/v1/product/**").permitAll()
-                                .requestMatchers("/api/v1/products").permitAll()
-                                .requestMatchers("/api/v1/random").permitAll()
-                                .requestMatchers("/api/v1/ranking").permitAll()
-                                .requestMatchers("/api/v1/ranking11").permitAll()
-
-                                //Order N/A
-                                //비회원 주문 없음
-
-                                //Cart N/A
-                                //비회원 장바구니 없음
-
-                                //Review
-                                .requestMatchers("/api/v1/product-review/*/reviews").permitAll()
-                                .requestMatchers(HttpMethod.GET, "/api/v1/product-review/**").permitAll()
-
-                                //Option (permitAll)
-                                .requestMatchers("/api/v1/option/**").permitAll()
-
-                                //Like N/A
-                                //비회원 좋아요 없음
-
-                                //Coupon
-                                .requestMatchers(HttpMethod.GET, "/api/v1/coupon").permitAll()
-
-                                //Category (permitAll)
-                                .requestMatchers("/api/v1/category").permitAll()
-                                .requestMatchers("/api/v1/category-child").permitAll()
-                                .requestMatchers("/api/v1/category-product").permitAll()
-
-                                //Bundle (permitAll)
-                                .requestMatchers("/api/v1/bundles").permitAll()
-                                .requestMatchers("/api/v1/bundles/**").permitAll()
-
-                                //Brand (permitAll)
-                                .requestMatchers("/api/v1/brand").permitAll()
-                                .requestMatchers("/api/v1/brand/**").permitAll()
-
-                                //Search
-                                .requestMatchers("/api/v1/search").permitAll()
-                                .requestMatchers("/api/v1/search-list").permitAll()
-
-                                //Admin (permit All)
-                                //현재 Admin 인증 진행 하지 않음
-                                //todo : Admin 회원 인증 가능 하면 삭제 필요.
-                                .requestMatchers("/api/v1/admin/**").permitAll()
-
-
-
-
-
-
-
-
-                                //V2 동일하게 처리
-                                //Auth
-                                .requestMatchers("/api/v2/auth/check-duplicate-id").permitAll()
-                                .requestMatchers("/api/v2/auth/find-id/**").permitAll()
-                                .requestMatchers("/api/v2/auth/login").permitAll()
-                                .requestMatchers("/api/v2/auth/signup").permitAll()
-
-                                //Admin에서 회원 탈퇴 기능 사용하기 위해 일시적 허용
-                                //todo : Admin 회원 인증 가능 하면 삭제 필요.
-                                .requestMatchers("/api/v2/auth/secession/**").permitAll()
-
-                                //OAuth (permitAll)
-                                .requestMatchers("/api/v2/oauth/**").permitAll()
-
-                                //User (permitALl)
-                                .requestMatchers("/api/v2/users/**").permitAll()
-
-                                //Address N/A
-                                //비회원 주소 없음
-
-                                //Product
-                                .requestMatchers("/api/v2/product/**").permitAll()
-                                .requestMatchers("/api/v2/products").permitAll()
-                                .requestMatchers("/api/v2/random").permitAll()
-                                .requestMatchers("/api/v2/ranking").permitAll()
-                                .requestMatchers("/api/v2/ranking11").permitAll()
-
-                                //Order N/A
-                                //비회원 주문 없음
-
-                                //Cart N/A
-                                //비회원 장바구니 없음
-
-                                //Review
-                                .requestMatchers("/api/v2/product-review/*/reviews").permitAll()
-                                .requestMatchers(HttpMethod.GET, "/api/v2/product-review/**").permitAll()
-
-                                //Option (permitAll)
-                                .requestMatchers("/api/v2/option/**").permitAll()
-
-                                //Like N/A
-                                //비회원 좋아요 없음
-
-                                //Coupon
-                                .requestMatchers(HttpMethod.GET, "/api/v2/coupon").permitAll()
-
-                                //Category (permitAll)
-                                .requestMatchers("/api/v2/category").permitAll()
-                                .requestMatchers("/api/v2/category-child").permitAll()
-                                .requestMatchers("/api/v2/category-product").permitAll()
-
-                                //Bundle (permitAll)
-                                .requestMatchers("/api/v2/bundles").permitAll()
-                                .requestMatchers("/api/v2/bundles/**").permitAll()
-
-                                //Brand (permitAll)
-                                .requestMatchers("/api/v2/brand").permitAll()
-                                .requestMatchers("/api/v2/brand/**").permitAll()
-
-                                //Search
-                                .requestMatchers("/api/v2/search").permitAll()
-                                .requestMatchers("/api/v2/search-list").permitAll()
-
-                                //Admin (permit All)
-                                //현재 Admin 인증 진행 하지 않음
-                                //todo : Admin 회원 인증 가능 하면 삭제 필요.
-                                .requestMatchers("/api/v2/admin/**").permitAll()
+//                                //Auth
+//                                .requestMatchers("/api/v1/auth/check-duplicate-id").permitAll()
+//                                .requestMatchers("/api/v1/auth/find-id/**").permitAll()
+//                                .requestMatchers("/api/v1/auth/login").permitAll()
+//                                .requestMatchers("/api/v1/auth/signup").permitAll()
+//
+//                                //Admin에서 회원 탈퇴 기능 사용하기 위해 일시적 허용
+//                                //todo : Admin 회원 인증 가능 하면 삭제 필요.
+//                                .requestMatchers("/api/v1/auth/secession/**").permitAll()
+//
+//                                //OAuth (permitAll)
+//                                .requestMatchers("/api/v1/oauth/**").permitAll()
+//
+//                                //User (permitALl)
+//                                .requestMatchers("/api/v1/users/**").permitAll()
+//
+//                                //Address N/A
+//                                //비회원 주소 없음
+//
+//                                //Product
+//                                .requestMatchers("/api/v1/product/**").permitAll()
+//                                .requestMatchers("/api/v1/products").permitAll()
+//                                .requestMatchers("/api/v1/random").permitAll()
+//                                .requestMatchers("/api/v1/ranking").permitAll()
+//                                .requestMatchers("/api/v1/ranking11").permitAll()
+//
+//                                //Order N/A
+//                                //비회원 주문 없음
+//
+//                                //Cart N/A
+//                                //비회원 장바구니 없음
+//
+//                                //Review
+//                                .requestMatchers("/api/v1/product-review/*/reviews").permitAll()
+//                                .requestMatchers(HttpMethod.GET, "/api/v1/product-review/**").permitAll()
+//
+//                                //Option (permitAll)
+//                                .requestMatchers("/api/v1/option/**").permitAll()
+//
+//                                //Like N/A
+//                                //비회원 좋아요 없음
+//
+//                                //Coupon
+//                                .requestMatchers(HttpMethod.GET, "/api/v1/coupon").permitAll()
+//
+//                                //Category (permitAll)
+//                                .requestMatchers("/api/v1/category").permitAll()
+//                                .requestMatchers("/api/v1/category-child").permitAll()
+//                                .requestMatchers("/api/v1/category-product").permitAll()
+//
+//                                //Bundle (permitAll)
+//                                .requestMatchers("/api/v1/bundles").permitAll()
+//                                .requestMatchers("/api/v1/bundles/**").permitAll()
+//
+//                                //Brand (permitAll)
+//                                .requestMatchers("/api/v1/brand").permitAll()
+//                                .requestMatchers("/api/v1/brand/**").permitAll()
+//
+//                                //Search
+//                                .requestMatchers("/api/v1/search").permitAll()
+//                                .requestMatchers("/api/v1/search-list").permitAll()
+//
+//                                //Admin (permit All)
+//                                //현재 Admin 인증 진행 하지 않음
+//                                //todo : Admin 회원 인증 가능 하면 삭제 필요.
+//                                .requestMatchers("/api/v1/admin/**").permitAll()
+//
+//
+//
+//
+//
+//
+//
+//
+//                                //V2 동일하게 처리
+//                                //Auth
+//                                .requestMatchers("/api/v2/auth/check-duplicate-id").permitAll()
+//                                .requestMatchers("/api/v2/auth/find-id/**").permitAll()
+//                                .requestMatchers("/api/v2/auth/login").permitAll()
+//                                .requestMatchers("/api/v2/auth/signup").permitAll()
+//
+//                                //Admin에서 회원 탈퇴 기능 사용하기 위해 일시적 허용
+//                                //todo : Admin 회원 인증 가능 하면 삭제 필요.
+//                                .requestMatchers("/api/v2/auth/secession/**").permitAll()
+//
+//                                //OAuth (permitAll)
+//                                .requestMatchers("/api/v2/oauth/**").permitAll()
+//
+//                                //User (permitALl)
+//                                .requestMatchers("/api/v2/users/**").permitAll()
+//
+//                                //Address N/A
+//                                //비회원 주소 없음
+//
+//                                //Product
+//                                .requestMatchers("/api/v2/product/**").permitAll()
+//                                .requestMatchers("/api/v2/products").permitAll()
+//                                .requestMatchers("/api/v2/random").permitAll()
+//                                .requestMatchers("/api/v2/ranking").permitAll()
+//                                .requestMatchers("/api/v2/ranking11").permitAll()
+//
+//                                //Order N/A
+//                                //비회원 주문 없음
+//
+//                                //Cart N/A
+//                                //비회원 장바구니 없음
+//
+//                                //Review
+//                                .requestMatchers("/api/v2/product-review/*/reviews").permitAll()
+//                                .requestMatchers(HttpMethod.GET, "/api/v2/product-review/**").permitAll()
+//
+//                                //Option (permitAll)
+//                                .requestMatchers("/api/v2/option/**").permitAll()
+//
+//                                //Like N/A
+//                                //비회원 좋아요 없음
+//
+//                                //Coupon
+//                                .requestMatchers(HttpMethod.GET, "/api/v2/coupon").permitAll()
+//
+//                                //Category (permitAll)
+//                                .requestMatchers("/api/v2/category").permitAll()
+//                                .requestMatchers("/api/v2/category-child").permitAll()
+//                                .requestMatchers("/api/v2/category-product").permitAll()
+//
+//                                //Bundle (permitAll)
+//                                .requestMatchers("/api/v2/bundles").permitAll()
+//                                .requestMatchers("/api/v2/bundles/**").permitAll()
+//
+//                                //Brand (permitAll)
+//                                .requestMatchers("/api/v2/brand").permitAll()
+//                                .requestMatchers("/api/v2/brand/**").permitAll()
+//
+//                                //Search
+//                                .requestMatchers("/api/v2/search").permitAll()
+//                                .requestMatchers("/api/v2/search-list").permitAll()
+//
+//                                //Admin (permit All)
+//                                //현재 Admin 인증 진행 하지 않음
+//                                //todo : Admin 회원 인증 가능 하면 삭제 필요.
+//                                .requestMatchers("/api/v2/admin/**").permitAll()
 
                                 .anyRequest()
                                 .authenticated()
