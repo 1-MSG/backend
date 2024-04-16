6ea1ab48c2e13b9ef2de25b54217d30107bf5b89
KangBaekGwa <ksu9801@gmail.com>
2024-04-16 14:27:27 +0900

    fix : Like Part V1 Api 삭제

--- src/main/java/spharos/msg/domain/likes/controller/LikesController.java
+++ src/main/java/spharos/msg/domain/likes/controller/LikesController.java
@@ -7,17 +7,17 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import spharos.msg.domain.likes.dto.IsLikesDto;
-import spharos.msg.domain.likes.service.LikesServiceV1;
+import spharos.msg.domain.likes.service.LikesServiceV2;
 import spharos.msg.global.api.ApiResponse;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/like")
-@Tag(name = "Likes V1", description = "좋아요 API")
-public class LikesControllerV1 {
-    private final LikesServiceV1 likesServiceV1;
+@RequestMapping("/api/v2/like")
+@Tag(name = "Likes", description = "좋아요 API")
+public class LikesController {
+    private final LikesServiceV2 likesServiceV2;
 
     @Operation(summary = "상품 좋아요 등록",
             description = "상품에 좋아요를 등록합니다.")
@@ -25,7 +25,7 @@ public class LikesControllerV1 {
     private ApiResponse<Void> likeProduct(
             @PathVariable Long productId,
             @AuthenticationPrincipal UserDetails userDetails) {
-        return likesServiceV1.likeProduct(productId, userDetails.getUsername());
+        return likesServiceV2.likeProduct(productId, userDetails.getUsername());
     }
     @Operation(summary = "상품 좋아요 해제",
             description = "상품에 등록된 좋아요를 해제합니다.")
@@ -33,7 +33,7 @@ public class LikesControllerV1 {
     private ApiResponse<Void> deleteLikeProduct(
             @PathVariable Long productId,
             @AuthenticationPrincipal UserDetails userDetails) {
-        return likesServiceV1.deleteLikeProduct(productId, userDetails.getUsername());
+        return likesServiceV2.deleteLikeProduct(productId, userDetails.getUsername());
     }
     @Operation(summary = "좋아요한 상품 목록 조회",
             description = "좋아요 등록된 상품들을 조회합니다.")
@@ -41,7 +41,7 @@ public class LikesControllerV1 {
     private ApiResponse<List<Long>> getProductLikeList(
             @AuthenticationPrincipal UserDetails userDetails
     ) {
-        return likesServiceV1.getProductLikeList(userDetails.getUsername());
+        return likesServiceV2.getProductLikeList(userDetails.getUsername());
     }
 
     @Operation(summary = "상품별 좋아요 조회",
@@ -51,6 +51,6 @@ public class LikesControllerV1 {
             @AuthenticationPrincipal UserDetails userDetails,
             @PathVariable Long productId
     ) {
-        return likesServiceV1.getProductLike(userDetails.getUsername(),productId);
+        return likesServiceV2.getProductLike(userDetails.getUsername(),productId);
     }
 }
