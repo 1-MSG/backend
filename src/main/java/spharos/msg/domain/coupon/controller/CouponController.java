32fbcfbe716a38f7efbaedd876658292d227e422
KangBaekGwa <ksu9801@gmail.com>
2024-04-16 14:20:09 +0900

    fix : Coupon V1/V2 분기

--- src/main/java/spharos/msg/domain/coupon/controller/CouponController.java
+++ src/main/java/spharos/msg/domain/coupon/controller/CouponController.java
@@ -1,22 +1,25 @@
 package spharos.msg.domain.coupon.controller;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import spharos.msg.domain.coupon.dto.CouponResponseDto;
-import spharos.msg.domain.coupon.service.CouponServiceV1;
+import spharos.msg.domain.coupon.service.CouponServiceV2;
 import spharos.msg.global.api.ApiResponse;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1")
-@Tag(name = "Coupon V1", description = "쿠폰 API")
-public class CouponControllerV1 {
-    private final CouponServiceV1 couponService;
+@RequestMapping("/api/v2")
+@Tag(name = "Coupon", description = "쿠폰 API")
+public class CouponController {
+    private final CouponServiceV2 couponService;
 
     @GetMapping("/coupon")
     private ApiResponse<List<CouponResponseDto>> getCoupon(
