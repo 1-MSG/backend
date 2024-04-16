ff1291722015b8bec5ebcc77f68b24e5a237c4c7
chojunho <chojunho0310@naver.com>
2024-04-16 10:33:42 +0900

    fix : 옵션 예외 처리 수정

--- src/main/java/spharos/msg/domain/options/service/OptionsServiceV1.java
+++ src/main/java/spharos/msg/domain/options/service/OptionsServiceV1.java
@@ -25,7 +25,6 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import static java.util.stream.Collectors.toList;
 import static spharos.msg.domain.options.converter.OptionsConverter.toDto;
 import static spharos.msg.domain.options.converter.OptionsConverter.toNameDto;
 
